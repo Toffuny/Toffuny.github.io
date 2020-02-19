aa72e85f8f68aff2e0cba47a413fd4de507a57a4
Toffuny <32074605+Toffuny@users.noreply.github.com>
2020-02-19 10:57:37 -0800

    change type

--- js/script.js
+++ js/script.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
 	setTimeout( function(){
 		if($("#site-type").length) {
         $(".typewrite span").typed({
-            strings: ["portfolio ", "interdisciplinary work", "multimedianess-ish", "place"],
+            strings: ["multimedia", "video editor"],
             typeSpeed: 100,
             backDelay: 500,
             loop: false,
